0f26036da0c1e5469fa66cd04424f343758cfa98
stevel705 <stevel705@outlook.com>
2023-12-11 22:22:45 +0400

    added print and get from pipe todo

--- todo.go
+++ todo.go
@@ -1,16 +1,19 @@
 package todo
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
+	"io"
 	"os"
+	"strings"
 	"time"
 )
 
 type item struct {
-	Task 	  string
-	Done 	  bool
-	CreatedAt time.Time
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
 	CompletedAt time.Time
 }
 
@@ -20,10 +23,10 @@ func (t *Todos) Add(task string) {
 	*t = append(*t, item{Task: task, Done: false, CreatedAt: time.Now(), CompletedAt: time.Time{}})
 }
 
-func (t *Todos) Complete(index int) error{
+func (t *Todos) Complete(index int) error {
 	ls := *t
 	if index <= 0 || index > len(ls) {
-		return errors.New("invalid index") 
+		return errors.New("invalid index")
 	}
 
 	ls[index-1].Done = true
@@ -51,7 +54,7 @@ func (t *Todos) Load(filename string) error {
 	}
 
 	err = json.Unmarshal(file, t)
-	
+
 	if err != nil {
 		return err
 	}
@@ -71,4 +74,37 @@ func (t *Todos) Store(filename string) error {
 	}
 
 	return nil
+}
+
+func (t *Todos) Print() {
+
+	for i, item := range *t {
+		prefix := " "
+		if item.Done {
+			prefix = "X"
+		}
+		println(i+1, prefix, item.Task)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("task cannot be blank")
+	}
+
+	return text, nil
 }
